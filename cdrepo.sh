c2d3bff3c82ddcfe411091413d1be9a8d619b9d9
msdlr <miguelsrosa97@gmail.com>
2020-04-21 22:45:49 +0200

    change to repo script

---
+++ cdrepo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+repodir=$(find . -name '.git' | sed 's|^\.\/||g  ; s|\/.git$||' | fzy -l `tput lines`)
+
+# git status and files
+cd $repodir
+git status && ls -l 
+
+# Execute shell on that directory
+`echo $0`
+exit
