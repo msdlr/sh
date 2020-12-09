1eca4a2df814c27f62804ada4b6894bde63ac6a3
msdlr <miguelsrosa97@gmail.com>
2020-12-09 13:22:29 +0100

    bulk images -> comic book (tar)

---
+++ cbt.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Remove spaces in filenames
+find -name "* *" -type d | rename 's/ //g'
+
+dirs=$(ls -d */ | sort | sed -e 's|\/||g')
+
+for dirn in $dirs;
+do
+	tar -czvf $dirn.cbt $dirn/*
+done
+exit
