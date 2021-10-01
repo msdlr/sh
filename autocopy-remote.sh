4f3e649f847667e06255776388d9176f806e2a00
msdlr <miguelsrosa97@gmail.com>
2021-10-01 13:23:57 +0200

    cleanup

--- autocopy-remote.sh
+++ autocopy-remote.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
 
-# Trackea cambios en un repo local y lo copia en cellia por scp
-# Uso: $0 user_cellia repo_local
+# Trackea changes in local repos and updates remote copies
+# Uso: $0 remote_user local_dir 
 
 # Valores por defecto
 USER_REMOTE=$USER
-LOCAL_REPO=/path/to/repo
+LOCAL_DIR=/path/to/repo/
 DEST=server
 
 [ ! -z $1 ] && USER_REMOTE=$1
 [ ! -z $2 ] && DEST=$2
-[ ! -z $3 ] && LOCAL_REPO=$3
-PATH_REMOTE=$(echo $LOCAL_REPO | sed "s/$USER/$USER_REMOTE/g")
+[ ! -z $3 ] && LOCAL_DIR=$3
 
-# Dependencias: entr, rsync
-which entr || sudo apt install entr -y 
-which rsync || sudo apt install rsync -y 
+# Remove trailing '/' from autocompletion
+LOCAL_DIR=$(echo $LOCAL_DIR | sed 's|/$||')
 
-echo "$USER -> $USER_REMOTE ($PATH_REMOTE)"
+PATH_REMOTE=$(basename $(echo $LOCAL_DIR | sed "s/$USER/$USER_REMOTE/g"))
+
+# Dependencies: entr, rsync
+inst='sudo apt install -y'
+which entr || $inst install entr
+which rsync || $inst install rsync
 
 ssh $USER_REMOTE@$DEST [ -d $PATH_REMOTE ] || ssh $USER_REMOTE@$DEST mkdir -pv $PATH_REMOTE
 
 while [ "1" = "1" ];
 do
-	ls $LOCAL_REPO/* | entr rsync -rv $LOCAL_REPO $DEST:$PATH_REMOTE
+	find $LOCAL_DIR | entr rsync -rv $LOCAL_DIR $DEST:$PATH_REMOTE
+	exit
 done
