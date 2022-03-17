b6e910797d37d3eb73db86f771530241e06a123e
msdlr <miguelsrosa97@gmail.com>
2022-03-17 09:21:59 +0100

    Autocopy script notification

--- autocopy-remote.sh
+++ autocopy-remote.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Trackea changes in local repos and updates remote copies
+# Track changes in local repos and updates remote copies
 # Uso: $0 remote_user local_dir 
 
-# Valores por defecto
-USER_REMOTE=$USER
+# Default values
+USER_REMOTE=${USER}
 LOCAL_DIR=/path/to/repo/
 DEST=server
 
@@ -13,19 +13,19 @@ DEST=server
 [ ! -z $3 ] && LOCAL_DIR=$3
 
 # Remove trailing '/' from autocompletion
-LOCAL_DIR=$(echo $LOCAL_DIR | sed 's|/$||')
+LOCAL_DIR=$(echo ${LOCAL_DIR} | sed 's|/$||')
 
-PATH_REMOTE=$(dirname $(echo $LOCAL_DIR | sed "s/$USER/$USER_REMOTE/g"))
+PATH_REMOTE=$(dirname $(echo ${LOCAL_DIR} | sed "s/${USER}/${USER_REMOTE}/g"))
 
 # Dependencies: entr, rsync
 inst='sudo apt install -y'
 which entr || $inst install entr
 which rsync || $inst install rsync
 
-ssh $USER_REMOTE@$DEST [ -d $PATH_REMOTE ] || ssh $USER_REMOTE@$DEST mkdir -pv $PATH_REMOTE
+ssh ${USER_REMOTE}@${DEST} [ -d ${PATH_REMOTE} ] || ssh ${USER_REMOTE}@${DEST} mkdir -pv ${PATH_REMOTE}
 
 while [ "1" = "1" ];
 do
-	find $LOCAL_DIR | entr rsync -rv $LOCAL_DIR $DEST:$PATH_REMOTE
+	find ${LOCAL_DIR} | entr sh -c "rsync -rv ${LOCAL_DIR} ${DEST}:${PATH_REMOTE} && notify-send \"Autocopy\" \"$(basename ${LOCAL_DIR}) ➡ ${PATH_REMOTE}@${DEST}\""
 	exit
 done
