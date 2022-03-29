28adc04fcdb7adfe61611a7487df4c76a30aeb4f
msdlr <miguelsrosa97@gmail.com>
2022-03-29 08:38:58 +0200

    Notification in autocopy script

--- autocopy-remote.sh
+++ autocopy-remote.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
 # Track changes in local repos and updates remote copies
-# Uso: $0 remote_user local_dir 
+# Use: $0 remote_user remote_server local_dir 
 
-# Default values
-USER_REMOTE=${USER}
-LOCAL_DIR=/path/to/repo/
-DEST=server
-
-[ ! -z $1 ] && USER_REMOTE=$1
-[ ! -z $2 ] && DEST=$2
-[ ! -z $3 ] && LOCAL_DIR=$3
+USER_REMOTE=$1
+DEST=$2
+LOCAL_DIR=$3
 
 # Remove trailing '/' from autocompletion
 LOCAL_DIR=$(echo ${LOCAL_DIR} | sed 's|/$||')
@@ -19,13 +14,12 @@ PATH_REMOTE=$(dirname $(echo ${LOCAL_DIR} | sed "s/${USER}/${USER_REMOTE}/g"))
 
 # Dependencies: entr, rsync
 inst='sudo apt install -y'
-which entr || $inst install entr
-which rsync || $inst install rsync
+which entr >/dev/null || $inst install entr
+which rsync >/dev/null || $inst install rsync
 
 ssh ${USER_REMOTE}@${DEST} [ -d ${PATH_REMOTE} ] || ssh ${USER_REMOTE}@${DEST} mkdir -pv ${PATH_REMOTE}
 
 while [ "1" = "1" ];
 do
-	find ${LOCAL_DIR} | entr sh -c "rsync -rv ${LOCAL_DIR} ${DEST}:${PATH_REMOTE} && notify-send \"Autocopy\" \"$(basename ${LOCAL_DIR}) ➡ ${PATH_REMOTE}@${DEST}\""
-	exit
+	find ${LOCAL_DIR} | entr sh -c "rsync -rv --delete ${LOCAL_DIR} ${DEST}:${PATH_REMOTE} && notify-send \"Autocopy\" \"$(basename ${LOCAL_DIR}) ➡ ${PATH_REMOTE}@${DEST}\""
 done
