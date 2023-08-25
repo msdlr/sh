1e8e9f0ab9e478a3ab84ff8030704286f3c9b9ce
msdlr <miguelsrosa97@gmail.com>
2023-08-25 12:04:08 +0200

    Cleanup script now also cleans cache by default

--- clean7days.sh
+++ clean7days.sh
@@ -3,7 +3,9 @@
 # Selects downloads directory
 . ~/.config/user-dirs.dirs
 
-dir=${XDG_DOWNLOAD_DIR}
+dirs="${XDG_DOWNLOAD_DIR} ${XDG_CACHE_DIR}"
+
+[ -z "$dirs" ] && [ -z "$1" ] && exit
 
 # Otherwise, the parameter
 
@@ -13,8 +15,11 @@ dir=${XDG_DOWNLOAD_DIR}
 
 [ -z "$2" ] && days="7" || days="$2"
 
-# Find files and delete them (Not the directry itself)
-find ${dir}/ -type f -mtime +${days} -delete
-find ${dir}/ -mindepth 1 -depth -type d -empty -delete
+for dir in $dirs
+do
+	# Find files and delete them (Not the directry itself)
+	find ${dir}/ -type f -mtime +${days} -delete
+	find ${dir}/ -mindepth 1 -depth -type d -empty -delete
+done
 
 exit
