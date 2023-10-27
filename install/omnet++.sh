cb73fe9ecc5366b636b48d46d20bdb384fb88fd7
msdlr <miguelsrosa97@gmail.com>
2023-10-27 22:56:36 +0200

    Get omnet through git clone

--- install/omnet++.sh
+++ install/omnet++.sh
@@ -3,7 +3,7 @@
 # Dependencies: curl
 
 DEST_PREFIX=${DEST_PREFIX:="/opt"}
-SCRATCH_DIR=${SCRATCH_DIR:="/tmp"}
+OMNET_ROOT=${OMNET_ROOT:=${DEST_PREFIX}/omnetpp}
 exit=""
 
 for dep in curl fzf
@@ -20,40 +20,37 @@ $exit
 inst_deps () {
     sudo apt install -y python3 python3-pip cmake libopenmpi* openmpi-*
 
-    sudo apt-get install build-essential clang lld gdb bison flex perl qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5opengl5-dev libxml2-dev zlib1g-dev doxygen graphviz libwebkit2gtk-4.0-37 -y &
+    sudo apt-get install build-essential gdb bison flex perl qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5opengl5-dev libxml2-dev zlib1g-dev doxygen graphviz libwebkit2gtk-4.0-37 -y &
     python3 -m pip install --user --upgrade numpy pandas matplotlib scipy seaborn posix_ipc &
     wait
 }
 
-dl_tgz () {
-    curl -s https://omnetpp.org/download/ | grep -o "\"http.*$(uname -p).tgz\"" | sed 's/"//g' > /tmp/omnetpp_archives
-    curl -s https://omnetpp.org/download/old | grep -o "\"http.*$(uname -p).tgz\"" | sed 's/"//g' >> /tmp/omnetpp_archives
-    tgz_link=$(cat /tmp/omnetpp_archives | fzf)
-    
-    cd ${SCRATCH_DIR}
-    [ -f $(basename ${tgz_link}) ] || wget ${tgz_link}
-}
-
-extract () {
-    cd /tmp
-    tar -xzf "${SCRATCH_DIR}/$(basename ${tgz_link})"
-    omnet_root="$(find ${PWD} -maxdepth 1 -type d -name 'omnet*' | head -n 1)"
-    sudo mv ${omnet_root} ${DEST_PREFIX}
-    omnet_root="${DEST_PREFIX}/$(basename ${omnet_root})"
-    sudo chown -R ${USER}:${USER} ${omnet_root}
-}
-
-configure () {
-    cd ${omnet_root}
+configure_make () {
+    cd ${OMNET_ROOT}
+    VER=$(git --no-pager tag --list | tac | fzf)
+    git checkout ${VER}
+    cp configure.user.dist configure.user
     . ./setenv
     ./configure WITH_OSG=no 
-    make -j$(getconf _NPROCESSORS_ONLN) all
+    make -j1 all
+}
+
+clone_omnet () {
+    if [ ! -d ${OMNET_ROOT} ]
+    then
+        mkdir -p ${OMNET_ROOT} || (sudo mkdir -p ${OMNET_ROOT} && sudo chown ${USER} ${OMNET_ROOT})
+        git clone https://github.com/omnetpp/omnetpp.git ${OMNET_ROOT}
+    elif [ -f ${OMNET_ROOT}.git ]
+    then
+        cd ${OMNET_ROOT}
+        git pull
+        cd -
+    fi
 }
 
 inst_deps
-dl_tgz
-extract
-configure
+clone_omnet
+configure_make
 
-rm -i ${SCRATCH_DIR}/$(basename ${tgz_link})
+
 echo "Done!"
