e711f964c4f6c9e9f2684deecc29d23f5f3608ab
msdlr <miguelsrosa97@gmail.com>
2024-01-11 16:04:47 +0100

    Flatpak installation in configuration script

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -7,6 +7,24 @@ remotes=(
   "fedora oci+https://registry.fedoraproject.org"
 )
 
+# Debian-based
+if [ "$(command -v apt)" >/dev/null != "" ]
+then
+  sudo apt install flatpak -y
+fi
+
+# Arch-based
+if [ "$(command -v pacman)" >/dev/null != "" ]
+then
+  sudo pacman -S flatpak --noconfirm
+fi
+
+# RPM-based
+if [ "$(command -v dnf)" >/dev/null != "" ]
+then
+  sudo dnf install flatpak -y
+fi
+
 for remote in "${remotes[@]}"; do
   read -p "Do you want to add $remote to Flatpak? (y/n) " answer
   if [ "$answer" = "y" ]; then
