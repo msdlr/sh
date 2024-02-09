61b53ca24356add6637acb539fc5a0bd9acbdb59
msdlr <miguelsrosa97@gmail.com>
2024-02-09 09:52:55 +0100

    Script for installing Firefox with native apt package in Ubuntu

---
+++ install/firefox-ppa.sh
@@ -0,0 +1,30 @@
+ #!/usr/bin/env sh
+ 
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script requires root privileges. Restarting with sudo..."
+    sudo "$(realpath $0)" "$@"
+    exit $?
+else
+    # Your script's main logic goes here
+    echo "You are root! Proceeding with the script."
+fi
+
+# Remove snap
+snap remove firefox
+apt remove firefox -y 
+
+
+install -d -m 0755 /etc/apt/keyrings
+
+# Get keyring and add source
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" > /etc/apt/sources.list.d/mozilla.list
+
+# Pin package
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+
+sudo apt update -y && sudo apt install --reinstall firefox -y
